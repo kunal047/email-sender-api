181c2516ff37a68cacbbead61dc5f451f95fe73c
Kunal Chauhan <chauhan.kunal4@gmail.com>
2023-05-20 18:37:20 +0530

    fix: update origin for cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://smartlead.0xkrc.dev'],
     methods: 'GET,PUT,POST,DELETE,OPTIONS,PATCH',
     credentials: true,
   });
